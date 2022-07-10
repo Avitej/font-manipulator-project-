62d7e19420dee76f502d1b8d4ab4563f1e7cf6a8
Avitej <82682098+Avitej@users.noreply.github.com>
2022-07-10 15:35:29 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -11,12 +11,28 @@ function setup() {
 function modelLoaded() {
     console.log("posenet is initialized");
 }
+
 function draw() {
     background('#969A97');
+    textSize(32);
+    fill(0, 102, 153);
+    text('word', 30, 300);
 }
-function gotPoses() {
+function gotPoses(results) {
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+        
     }
+    textSize(difference);
+
 }
